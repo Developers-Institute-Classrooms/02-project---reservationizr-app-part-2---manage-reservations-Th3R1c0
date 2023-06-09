5a8fab93c9e6bfb06d49712f84e7ef89bfe4485b
rico saunders <RicoTS114@gmail.com>
2023-06-09 17:48:53 +1200

    auth, testing, backend, 3/4 frontend completed

--- client/src/components/CreateReservation.js
+++ client/src/components/CreateReservation.js
@@ -4,9 +4,60 @@ import DatePicker from "react-datepicker";
 import { useAuth0 } from "@auth0/auth0-react";
 import "react-datepicker/dist/react-datepicker.css";
 import "./CreateReservation.css";
-
+import { formatDate } from "../utils/formatDate";
 const CreateReservation = ({ restaurantName }) => {
-  return <></>;
+  const [date, setDate] = useState(new Date());
+  const [partySize, setPartySize] = useState(1);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorStatus, setErrorStatus] = useState(false);
+  const { getAccessTokenSilently } = useAuth0();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    const accessToken = await getAccessTokenSilently();
+
+    const reservation = {
+      partySize: partySize,
+      date: date,
+      restaurantName: restaurantName,
+    };
+
+    const response = await fetch("http://localhost:5001/reservations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(reservation),
+    });
+
+    if (!response.ok) {
+      setIsError(true);
+      setErrorStatus(response.status);
+    } else {
+      setIsLoading(false);
+      navigate("/");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="reservation-guest-input">Number of guests</label>
+      <input
+        className="reservation-guest-input"
+        type="text"
+        value={partySize}
+        onChange={(e) => setPartySize(e.target.value)}
+        required
+      />
+      <DatePicker selected={date} onChange={(date) => setDate(date)} />
+      <button className="submit-btn" type="submit" disabled={isLoading}>
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default CreateReservation;
